38ce156ba865c0f315421ae8f3bec4e1c55bbfa0
ImagneRabbits <mixxdarkz2@gmail.com>
2019-11-10 20:11:17 +0700

    [ADD] init project

---
+++ bank/index.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = 3000
+
+const ADMIN_ACCOUNT = {
+    username: 'admin',
+    password: 'admin'
+}
+
+const STATIC_PATH = path.join(__dirname, 'public');
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.cookie('SESSION', 'ABCDEFG')
+    res.sendFile(`${STATIC_PATH}/index.html`)
+})
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if (
+        (username && ADMIN_ACCOUNT.username == username) &&
+        (password && ADMIN_ACCOUNT.password == password)
+    ) {
+        res.send({
+            message: 'Success'
+        })
+    } else {
+        res.send({
+            message: 'Failed'
+        })
+    }
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
